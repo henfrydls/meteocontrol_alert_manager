73ed44036cf73635ccfd90477aecbf4fed32738e
Henfry De Los Santos <henfry.delossantos@gmail.com>
2022-03-13 22:27:36 -0400

    SQLite implemented for users login info

--- setting.py
+++ setting.py
@@ -1,10 +1,13 @@
 """Settings file for managing login info and encrypting"""
 
 from cryptography.fernet import Fernet
-import re
+import sqlite3
 import pyperclip as pc
+import re
+
 
 SERVERS_DICT = {"imap.gmail.com":"Gmail"}
+TABLE_NAME = "Users"
 
 def welcome() -> None:
     """Welcomen message to setting portal"""
@@ -46,13 +49,17 @@ def password_manager(password : str) -> tuple:
     key = Fernet.generate_key()
 
     # Saving key on clipboard
-    pc.copy(str(key))
+    pc.copy(str(key)[2:-1])
 
     fernet = Fernet(key)
     encpassword = fernet.encrypt(password.encode())
 
+    print(f"\nYour password key have been copied to clipboard!") 
+    print(f"Key: {str(key)[2:-1]}")
+    print("Save it somewhere secure and private!")  
+
     # Return encpassword and key in tuple
-    return (encpassword, key, password)
+    return (str(encpassword)[2:-1], password)
 
 
 def conf_summary(email: str, server: str, password: str) -> bool:
@@ -68,38 +75,136 @@ def conf_summary(email: str, server: str, password: str) -> bool:
         choice = input("\nY/N? ")
         if choice.lower() == "y":
             return True
-        else:
+        elif choice.lower() == "n":
             return False
+
+
+def saving_info(email: str, server: str, server_name: str, encpasswd: str) -> None:
+    """Save provided mail, server, and password to a database"""
+
+    connection = sqlite3.connect("resources/users.sqlite")
+    # Creating a way to send and recieve commands
+    cur = connection.cursor()
+
+    cur.executescript(f"""CREATE TABLE IF NOT EXISTS {TABLE_NAME}
+                (user_id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
+                user TEXT UNIQUE,
+                server TEXT, 
+                server_name TEXT,
+                encpasswd TEXT
+                );
+                """)
+
+    cur.execute('''INSERT INTO Users 
+        (user, server, server_name, encpasswd)
+		VALUES (?, ?, ?, ?)''', (email, server, server_name, encpasswd, ))
+
+    connection.commit()
+    connection.close()
+    
+
+def show_registered_users() -> dict:
+    """Get user_id and user from table Users"""
+
+    connection = sqlite3.connect("resources/users.sqlite")
+    # Creating a way to send and recieve commands
+    cur = connection.cursor()
+    
+    cur.execute(f"SELECT name FROM sqlite_master WHERE type='table' AND name='{TABLE_NAME}';")
+    response = cur.fetchone()
+    if response != None:
+        users = dict()
+
+        cur.execute("SELECT COUNT(user_id) FROM Users")
+        registered_users = int(cur.fetchone()[0])
+        cur.execute(("SELECT user_id, user FROM Users"))
+
+        for _ in range(registered_users):
+            user = cur.fetchone()
+            users[user[0]] = user[1]
+        
+        connection.close()
+
+        return users
+    else:
+        return False
+
+
+def update_existing_password(user_id: int, encpassword: str) -> None:
+    """Update password"""
+
+    connection = sqlite3.connect("resources/users.sqlite")
+    # Creating a way to send and recieve commands
+    cur = connection.cursor() 
+
+    cur.execute("UPDATE Users SET encpasswd = ? WHERE user_id = ?", (encpassword, user_id))
+    connection.commit()
+    connection.close()
+
 
 def settings_loop() -> None:
     """Set email and password, change password and display information."""
 
     ACTIVE : bool = True
 
-    while ACTIVE:
+    while True:
         options = input("""
         What do you want to do?
         1. Set email and password
         2. Change password
-        3. Show previous login info
-        """)
+        3. Show all login info
+        \nResponse: """)
+        print("\n")
 
         if options == "1":
-            print("\nWARNING: All previous login info will be lost.\n")
+            print("\nWARNING: Login info will be replaced.\n")
             while ACTIVE:
                 EMAIL : str = input("\nEmail: ").strip().lower()
                 if email_checker(EMAIL):
                     SERVER: str = setting_server()
-                    ENCPASSWORD, KEY, PASSWORD = password_manager(input("\nPassword: "))
-                    print(f"\nYour password key have been copied to clipboard!") 
-                    print(f"Key: {KEY}")
-                    print("Save it somewhere secure and private!")  
+                    ENCPASSWORD, PASSWORD = password_manager(input("\nPassword: "))
                 else:
                     continue
                 if conf_summary(EMAIL, SERVER, PASSWORD):
-
+                    saving_info(EMAIL, SERVER, SERVERS_DICT[SERVER], ENCPASSWORD)
                     ACTIVE = False
 
-if __name__ == "__main__":
+        elif options == "2":
+            users = show_registered_users()
+            if users:
+                print("\nWARNING: A new encryting key will be generated.\n")
+                print("Choose an user_id.\n")
+                for user_id, user in users.items():
+                    print(f"{user_id} - {user}")
+
+                while True:
+                    response = int(input("\nResponse: "))
+                    if response not in list(users.keys()):
+                        print("Please select a valid user_id\n")
+                        continue
+                    break
+
+                new_password = input("\nNew password: ")
+                ENCPASSWORD, PASSWORD = password_manager(new_password)
+                update_existing_password(response, ENCPASSWORD)
+
+            else:
+                print("There are not users registered yet.\n")
+
+        elif options == "3":
+            users = show_registered_users()
+            if users:
+                for user_id, user in users.items():
+                    print(f"{user_id} - {user}")
+            else:
+                print("There are not users registered yet.\n")
+        
+        else:
+            break
+
+""" if __name__ == "__main__":
     welcome()
-    settings_loop()
+    settings_loop() """
+
+welcome()
+settings_loop()
