e8c6729a34b38b849051f9d7e87a46f9a2ff2421
Henfry De Los Santos <henfry.delossantos@gmail.com>
2022-03-22 23:59:28 -0400

    bug fixed

--- main.py
+++ main.py
@@ -1,26 +1,28 @@
+from email import message
 from resources.sql_manager import Data
 from resources.email_parser import Parse
 from datetime import datetime
+from time import sleep
 
 class Main:
     def __init__(self) -> None:
         self.PS = Parse()
-
         if not self.PS.info and self.PS.info is not None:
             print("""\nPlease go to setting.py and set/check your login info.\n""")
         else:
+            print("hey1")
             self.messages = self.PS.messages
         
             self.DT = Data()
             self.messages_read = self.DT.registered_messages()[0]
-            try:
+            if self.messages is not None:
                 if int(self.messages_read) == self.messages:
                     print("\nYou are up to date!\n")
-            except:
-                if self.messages_read is not None:
-                    self.PS.parsing(self.messages - int(self.messages_read))
                 else:
-                    self.PS.parsing(self.messages)
+                    messages_to_read = self.messages - int(self.messages_read)
+                    self.PS.parsing(self.messages, messages_to_read)
+            else:
+                self.PS.parsing(self.messages, self.messages)
 
             # Create a time stamp
             time = datetime.now()
@@ -31,5 +33,7 @@ class Main:
 
 
 if __name__ == "__main__":
-    Main()
-
+    while True:
+        main = Main()
+        del main
+        sleep(60)
